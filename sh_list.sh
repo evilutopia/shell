b13ff0b6af16d734fb840e84e5be4c980605438d
evilutopia <evilutopia@foxmail.com>
2017-10-07 09:16:26 -0500

    Update sh_list.sh

--- sh_list.sh
+++ sh_list.sh
@@ -13,15 +13,16 @@
 #f1 hehe string_def
 #f2 hehe string_def
 
+# 删除后的文件 file.txt #
+#2 string_def abc
+#5 string_def abc
+#f1 hehe string_def
+#f2 hehe string_def
+
 # 脚本
 cat file.txt| grep _def|sed 's/.* "\(.*_def\)" .*/\1/' \ # 找出名称字段
 |sort \ # 排序
 | uniq -u \ # 查找只出现过一次的名称
 |while read line ;do; sed -i .bak "/$line/d" file.txt ; done # 删除文件中名称只出现一次的行
 
-# 删除后的文件 file.txt #
-#2 string_def abc
-#5 string_def abc
-#f1 hehe string_def
-#f2 hehe string_def
 #######################################################################################
